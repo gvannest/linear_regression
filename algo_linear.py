117bbdb0f208f191c8335b12767fa6a6632a6621
gvannest <gautier.vanneste42@gmail.com>
2019-07-18 18:05:54 +0200

    currently trying to plot gradient descent live

--- algo_linear.py
+++ algo_linear.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+from class_plot import GraphLive
+
 class Algo:
 
 	flag_plot = False
@@ -18,71 +20,46 @@ class Algo:
 		mean_ = np.sum(self.X[:, 1:]) / self.X.shape[0]
 		range_ = np.max(self.X[:, 1:], axis=0) - np.min(self.X[:, 1:], axis=0)
 		X_norm = np.c_[self.X[:,0], (self.X[:, 1:] - mean_) / range_]
+		print(mean_)
 		return X_norm, mean_, range_
 
-	def predict(self):
-		return np.dot(self.X_norm, self.theta_norm)
+	def predict(self, X, theta):
+		return np.dot(X, theta)
 
-	def cost(self):
-		return np.sum(np.square(self.predict() - self.y)) / (2 * self.X.shape[0])
-
-	def live_plot1(self, fig, x_vec1, y1, line1, iter, pause_time=0.05):
-		if not line1:
-			plt.ion()
-			ax1 = fig.add_subplot(211)
-			ax1.set_ylim([0, self.cost()])
-			ax1.set_xlim([0, iter])
-			line1, = ax1.plot(x_vec1, y1, '-', alpha=0.8)
-			ax1.set_ylabel('J_history')
-			ax1.set_xlabel('Iterations')
-			ax1.set_title('Cost evolution')
-			plt.tight_layout(4, h_pad=3)
-			plt.show()
-		line1.set_ydata(y1)
-		plt.pause(pause_time)
-		return line1
-
-	def live_plot2(self, fig, x_vec2, y2, line2, pause_time=0.05):
-		if not line2:
-			plt.ion()
-			ax2 = fig.add_subplot(212)
-			line2, = ax2.plot(x_vec2, y2, '-', alpha=0.8)
-			ax2.scatter(self.X[:,1], self.y, c='blue')
-			ax2.set_ylabel('Price')
-			ax2.set_xlabel('Mileage')
-			ax2.set_title('Regression line')
-			plt.tight_layout(4, h_pad=3)
-			plt.show()
-		line2.set_ydata(y2)
-		plt.pause(pause_time)
-		return line2
+	def cost(self, X, theta):
+		return np.sum(np.square(self.predict(X, theta) - self.y)) / (2 * X.shape[0])
 
 	def fit_linear(self, alpha=1, iter=200):
 		m = self.X.shape[0]
+
 		if Algo.flag_plot:
-			plt.style.use('ggplot')
-			fig = plt.figure(figsize=(13, 13))
-			x_vec1 = np.arange(0, iter)
-			x_vec2 = np.array([0, np.max(self.X[:,1], axis=0)])
-			line1 = []
-			line2 = []
-			y1 = np.full(len(x_vec1), np.nan)
+			g1 = GraphLive(x_vec=np.arange(0, iter), y_vec=np.full(iter, np.nan),
+						   ax=GraphLive.fig.add_subplot(311), title="Real time cost evolution",
+						   x_label="Iterations", y_label="J_history")
+			g2 = GraphLive(x_vec=self.X[:,1], y_vec=self.y, ax=GraphLive.fig.add_subplot(312),
+						   title="Regression line", x_label="Mileage", y_label="Price")
+			g3 = GraphLive(x_vec=self.X_norm, y_vec=self.y, ax=GraphLive.fig.add_subplot(313),
+						   title="Gradient descent", x_label="theta1", y_label="theta0")
+
 		for i in range(iter):
-			diff = np.dot(self.predict() - self.y, self.X_norm)
+			diff = np.dot(self.predict(self.X_norm, self.theta_norm) - self.y, self.X_norm)
 			self.theta_norm = self.theta_norm - (alpha / m) * diff
-			self.J_history.append(self.cost())
+			self.J_history.append(self.cost(self.X_norm, self.theta_norm))
+
 			if Algo.flag_plot:
 				self.theta[0] = self.theta_norm[0] - self.theta_norm[1] * self.mean_ / self.range_
 				self.theta[1] = self.theta_norm[1] / self.range_
-				y2 = self.theta[0] + self.theta[1] * x_vec2
-				y1[i] = self.J_history[-1]
-				line1 = self.live_plot1(fig, x_vec1, y1, line1, iter)
-				line2 = self.live_plot2(fig, x_vec2, y2, line2)
+				g1.y_vec[i] = self.J_history[-1]
+				g1.live_line_evolution(y_limit=(0, self.cost(self.X_norm, self.theta_norm)), x_limit=(0, iter))
+				g2.live_regression(y_limit=(0, 1.2 * np.max(self.y, axis=0)),
+								   x_limit=(0, 1.2 * np.max(self.X[:,1], axis=0)),
+								   theta=self.theta)
+				g3.live_gd(self.cost)
+
 		self.theta[0] = self.theta_norm[0] - self.theta_norm[1] * self.mean_ / self.range_
 		self.theta[1] = self.theta_norm[1] / self.range_
 		if Algo.flag_plot:
-			plt.ioff()
-			plt.show()
+			GraphLive.close()
 
 		return None
 
--- class_plot.py
+++ class_plot.py
@@ -1,1 +1,67 @@
+import numpy as np
+
 import matplotlib.pyplot as plt
+
+class GraphLive:
+
+	graph_count = 0
+	plt.style.use('ggplot')
+	fig = plt.figure(figsize=(20, 13))
+
+	def __init__(self, x_vec=None, y_vec=None, ax=None, title=None, x_label=None, y_label=None):
+		self.x_vec = x_vec
+		self.y_vec = y_vec
+		self.line = []
+		self.ax = ax
+		self.title = title
+		self.x_label = x_label
+		self.y_label = y_label
+		GraphLive.graph_count += 1
+
+	def initialization(self, y_limit=None, x_limit=None):
+		self.ax.set_ylim(y_limit)
+		self.ax.set_xlim(x_limit)
+		self.ax.set_ylabel(self.y_label)
+		self.ax.set_xlabel(self.x_label)
+		self.ax.set_title(self.title)
+
+	def live_line_evolution(self, y_limit=None, x_limit=None, pause_time=0.005):
+		if not self.line:
+			plt.ion()
+			self.initialization(y_limit, x_limit)
+			self.line, = self.ax.plot(self.x_vec, self.y_vec, '-', alpha=0.8)
+			plt.tight_layout(4, h_pad=3)
+			plt.show()
+		self.line.set_ydata(self.y_vec)
+		plt.pause(pause_time)
+		return None
+
+	def live_regression(self, y_limit=None, x_limit=None, theta=None, pause_time=0.005):
+		x_reg = np.array([0, 1.2 * np.max(self.x_vec, axis=0)])
+		y_reg = theta[0] + theta[1] * x_reg
+		if not self.line:
+			plt.ion()
+			self.initialization(y_limit, x_limit)
+			self.line, = self.ax.plot(x_reg, y_reg, '-', alpha=0.8)
+			self.ax.scatter(self.x_vec, self.y_vec, c='blue')
+			plt.tight_layout(4, h_pad=3)
+			plt.show()
+		self.line.set_ydata(y_reg)
+		plt.pause(pause_time)
+		return None
+
+	def live_gd(self, f, pause_time=0.005):
+		max_y = np.max(self.y_vec, axis=0)
+		min_y = np.min(self.y_vec, axis=0)
+		y_range = abs(max_y - min_y)
+		theta0_range = np.arange(-1.2 * max_y, 1.2 * max_y)
+		theta1_range = np.arange(-y_range, y_range)
+
+		x1, x0 = np.meshgrid(theta1_range, theta0_range)
+		plt.contour(x1, x0, f(self.x_vec,(x1,x0)), 20, cmap='RdGy')
+
+	@classmethod
+	def close(cls):
+		plt.ioff()
+		plt.show()
+
